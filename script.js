e8b35665972fcdcc330f2b364a8f865dee68a0a4
officesuceavaoffice-eng <officesuceavaoffice@gmail.com>
2026-06-17 03:16:59 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
-console.log("Magron Cars Kft Website Loaded");
+function setLang(lang){
+
+document.querySelectorAll('[data-hu]').forEach(el=>{
+
+el.innerText=el.getAttribute(
+lang==='hu' ? 'data-hu' : 'data-en'
+);
+
+});
+
+}
+
+document.getElementById('bookingForm').addEventListener('submit',function(e){
+
+e.preventDefault();
+
+const name=document.getElementById('name').value;
+const phone=document.getElementById('phone').value;
+const email=document.getElementById('email').value;
+const pickup=document.getElementById('pickup').value;
+const destination=document.getElementById('destination').value;
+const date=document.getElementById('date').value;
+const passengers=document.getElementById('passengers').value;
+
+const msg=
+`Hello Magron Cars Kft
+
+Name: ${name}
+Phone: ${phone}
+Email: ${email}
+Pickup: ${pickup}
+Destination: ${destination}
+Date: ${date}
+Passengers: ${passengers}`;
+
+window.open(
+`https://wa.me/36206195189?text=${encodeURIComponent(msg)}`,
+'_blank'
+);
+
+});
